be1d07867db4f1d9de375aa47ff837dffc489e8c
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-27 22:24:01 +0900

    commit flex

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,19 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:nickname) }
-    it { is_expected.to validate_presence_of(:birth_day) }
-    it { is_expected.to validate_presence_of(:last_name) }
-    it { is_expected.to validate_presence_of(:first_name) }
-    it { is_expected.to validate_presence_of(:last_name_kana) }
-    it { is_expected.to validate_presence_of(:first_name_kana) }
-    it { is_expected.to allow_value('Abc123').for(:password) }
-    it { is_expected.not_to allow_value('abc123').for(:password).with_message('Include both letters and numbers') }
-    it { is_expected.not_to allow_value('あいうえお').for(:last_name).with_message('Input full-width characters') }
-    it { is_expected.not_to allow_value('あいうえお').for(:first_name).with_message('Input full-width characters') }
-    it { is_expected.not_to allow_value('アイウエオ').for(:last_name_kana).with_message('is invalid. Input full-width katakana characters') }
-    it { is_expected.not_to allow_value('アイウエオ').for(:first_name_kana).with_message('is invalid. Input full-width katakana characters') }
-  end
+  
 end  
 
 end
