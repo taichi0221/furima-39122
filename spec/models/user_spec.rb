b970068a01912174d56c39ba62e8635390b7a2a1
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-27 22:28:09 +0900

    testコード表示

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validations' do
-  
-end  
-
+  describe "ユーザー新規登録" do
+    it "nicknameが空だと登録できない" do
+    end
+    it "emailが空では登録できない" do
+    end
+  end
 end
