0d2cdba5a31788b804d35039644079f400aa6813
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-27 14:25:59 +0900

    migrate DB確認OK

--- db/migrate/20230327051218_devise_create_users.rb
+++ db/migrate/20230327051218_devise_create_users.rb
@@ -11,8 +11,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name,         null: false, default: ""
       t.string :last_name_kana,     null: false, default: ""
       t.string :first_name_kana,    null: false, default: ""
-      t.date   :birth_day,          null: false, default: ""
-      
+      t.date   :birth_day,          null: false
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
