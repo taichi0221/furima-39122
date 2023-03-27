b9cac5ce1996a9033ac8a492e6b441c35e8b926e
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-27 21:54:40 +0900

    Revert "factorybot facer準備"
    This reverts commit 3bf7141009d7d23e483a4335049d410fed060347.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
+  validates :nickname, presence: true  
+  validates :birth_day, presence: true  
+  validates :last_name, presence: true  
+  validates :first_name, presence: true  
+  validates :last_name_kana, presence: true  
+  validates :first_name_kana, presence: true  
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "Include both letters and numbers" }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Input full-width characters' }       
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Input full-width characters' }
+  validates :last_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "is invalid. Input full-width katakana characters" }
 end
