91e3c361a5c3cbcaf4fef7ca4e71c3d4df0c11c7
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-27 21:54:14 +0900

    Revert "miss commit"
    This reverts commit ca00c3bef72a46347428515e4503b1233cc528dd.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,4 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'factory_bot_rails'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
@@ -34,7 +33,7 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  I18n.locale = "en"
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
