0a3c49b2e43f32f0ed214f28e70d3d53546ef6ac
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-28 23:33:25 +0900

    商品出品　DB保存確認　renderで消えない

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,14 @@ class Item < ApplicationRecord
   belongs_to :delivery_charge
   has_one_attached :image
 
-  validates :category_id,        numericality: { other_than: 1 , message: "can't be blank"}
-  validates :situation_id,       numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id,      numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_day_id,    numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
-
+  validates :category_id, presence: true,       numericality: { other_than: 1 , message: "can't be blank"}
+  validates :situation_id, presence: true,      numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, presence: true,     numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, presence: true,   numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, presence: true,numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true,             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, format: { with: /\A[0-9]+\z/ }
+  validates :name, presence: true 
+  validates :explanation , presence: true
+  validates :image, presence: true
 end
