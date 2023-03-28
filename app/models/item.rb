ec56e49a1eb578b97b7ae78f097042ce045bfbec
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-28 18:17:06 +0900

    アクティブハッシュ設定　itemモデルバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :category
   belongs_to :situation
@@ -7,5 +8,10 @@ class Item < ApplicationRecord
   belongs_to :delivery_charge
   has_one_attached :image
 
+  validates :category_id,        numericality: { other_than: 1 , message: "can't be blank"}
+  validates :situation_id,       numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,      numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id,    numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 end
