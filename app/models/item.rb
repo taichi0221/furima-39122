fcd131991b75c380ad73c5c4547ff99bddfba7ae
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-28 15:37:08 +0900

    itemマイグレーション　アソシエーション追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
-
-
+  belongs_to :user
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :prefecture
+  belongs_to :shipping_day
+  belongs_to :delivery_charge
 
 
 
--- db/migrate/20230328062147_create_items.rb
+++ db/migrate/20230328062147_create_items.rb
@@ -1,21 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string     :name,             null: false, default: ""
-      t.text       :explanation,      null: false, default: ""
-      t.integer    :category_id       null: false, default: ""
+      t.string     :name,              null: false
+      t.text       :explanation,       null: false
+      t.integer    :category_id,       null: false
+      t.integer    :situation_id,      null: false
+      t.integer    :price,             null: false
+      t.integer    :prefecture_id,     null: false
+      t.integer    :shipping_day_id,   null: false
+      t.integer    :delivery_charge_id,null: false
+      t.references :user,              null: false, foreign_key: true
       t.timestamps
     end
   end
 end
-
-
-name              | string     | null: false                    |
-| explanation       | text       | null: false                    |
-| category_id       | integer    | null: false                    |
-| situation_id      | integer    | null: false                    |
-| price             | integer    | null: false                    |
-| prefecture_id     | integer    | null: false                    |
-| shipping_day_id   | integer    | null: false                    |
-| delivery_charge_id| integer    | null: false                    |
-| user              | references | null: false, foreign_key: true |
