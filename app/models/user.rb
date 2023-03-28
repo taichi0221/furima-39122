98897f2587f030d4f852fbd5c3dfa0329c86ea89
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-28 14:08:25 +0900

    コメントアウト削除、全角バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true  
   validates :first_name_kana, presence: true  
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "Include both letters and numbers" }
-  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Input full-width characters' }       
-  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Input full-width characters' }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters' }     
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters' }
   validates :last_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "is invalid. Input full-width katakana characters" }
   validates :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "is invalid. Input full-width katakana characters" }
 end
