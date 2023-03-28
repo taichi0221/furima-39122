74748b4714a7b899800fe3ca7690ba068bb6a83a
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-28 16:02:55 +0900

    has_one_attached itemストロングパラメーター

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,8 @@ class ItemsController < ApplicationController
   def index
   end
   
+
+  def item_params
+    params.require(:item).permit(:name, :explanation,:category_id,:situation_id,:price,:prefecture_id,:shipping_day_id,:delivery_charge_id, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
   belongs_to :delivery_charge
-
+  has_one_attached :image
 
 
 end
