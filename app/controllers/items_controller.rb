eb9e9e1814f5e6a5b338e45f693ad2f5ee4aeb9d
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-29 20:08:43 +0900

    move to index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -40,5 +41,12 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :explanation,:category_id,:situation_id,:price,:prefecture_id,:shipping_day_id,:delivery_charge_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user == @item.user
+      redirect_to action: :index
+    end
   end
 end
