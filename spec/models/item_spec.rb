9a29051b8e0bcf30b57c6cfc6b2927b7559adfe1
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-29 11:08:52 +0900

    item　出品OKテスト

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    @user = FactoryBot.build(:user)
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品が出品できる場合' do
+      it 'name,explanation,price,category_id,situation_id,prefecture_id,shipping_day_id,delivery_charge_id,imageが存在していれば保存できる' do
+        expect(@item).to be_valid
+      end
+    end
   end
 end
