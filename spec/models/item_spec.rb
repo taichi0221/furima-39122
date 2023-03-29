9f85b0d4c4e9210d2bedbc0ce97ec7cd4c588f0a
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-29 11:25:44 +0900

    id user OK

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -27,6 +27,37 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
+      it 'category_idが1(表記：---)では保存できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'situation_idが1(表記：---)では保存できない' do
+        @item.situation_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Situation can't be blank")
+      end
+      it 'prefecture_idが1(表記：---)では保存できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'shipping_day_idが1(表記：---)では保存できない' do
+        @item.shipping_day_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+      it 'delivery_charge_idが1(表記：---)では保存できない' do
+        @item.delivery_charge_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
+      end
+      it 'userが紐付いていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+      
       
     end
   end
