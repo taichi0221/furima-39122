ed3caaaafb6862728289ce074e54742224895548
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-30 10:34:40 +0900

    コメントアウト削除　ログアウト時の編集画面遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: [:edit]
   before_action :set_item, only: [:edit, :show, :update]
 
