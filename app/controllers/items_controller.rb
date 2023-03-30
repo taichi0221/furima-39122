76e4f3d7f765f618d7eec5bd79d38477505b6c49
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-30 10:24:32 +0900

    destroy削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new]
-  before_action :move_to_index, only: [:edit, :destroy]
+  before_action :move_to_index, only: [:edit]
   before_action :set_item, only: [:edit, :show, :update]
 
   def index
