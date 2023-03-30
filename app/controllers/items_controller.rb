e0dde811b0567271c0fce8fd55b681e41b6ef7f9
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-30 10:23:27 +0900

    可読性

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new]
   before_action :move_to_index, only: [:edit, :destroy]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -21,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
   
   def edit
-    @item = Item.find(params[:id])
   end
   
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -48,5 +46,9 @@ class ItemsController < ApplicationController
     unless user_signed_in? && current_user == @item.user
       redirect_to action: :index
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
