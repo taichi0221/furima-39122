4b05edc4b4070e23c68747c624e422454ec0bd13
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-31 18:09:11 +0900

    path通す　destroyアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, only: [:edit]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :move_to_index, only: [:edit, :destroy]
+  before_action :set_item, only: [:edit, :show, :update,]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -35,6 +35,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
   private
 
   def item_params
