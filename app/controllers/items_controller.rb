47b888c226088b3f2cc27a37308d4b38fb91a383
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-31 18:27:02 +0900

    ,削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: [:edit, :destroy]
-  before_action :set_item, only: [:edit, :show, :update,]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
