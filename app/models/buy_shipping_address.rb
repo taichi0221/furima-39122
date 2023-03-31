d23351d155edb65bbdbf3be88f8d93642b63a2a4
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-31 21:22:24 +0900

    formオブジェクト　バリデーション

--- app/models/buy_shipping_address.rb
+++ app/models/buy_shipping_address.rb
@@ -2,13 +2,14 @@ class BuyShippingAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id ,:post_code, :prefecture_id, :municipality, :address, :building, :tel_number, :buy_id
 
-  with_options presence: true do
-    validates :postal_code, presence: true,   format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-    validates :municipality, presence: true,
-    validates :address, presence: true,
-    validates :building
-    validates :tel_number, presence: true, length: { minimum: 10, maximum: 11 }, numericality: { only_integer: true }
-    
+  validates :user_id
+  validates :item_id
+  validates :postal_code, presence: true,   format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :municipality, presence: true,
+  validates :address, presence: true,
+  validates :building
+  validates :tel_number, presence: true, length: { minimum: 10, maximum: 11 }, numericality: { only_integer: true }
+  validates :buy_id
 
 end
