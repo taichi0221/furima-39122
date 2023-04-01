f3f844c0abb11a0eccb762e9149ac47f186c44f9
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-04-01 20:30:27 +0900

    buy_id バリデ削除

--- spec/models/buy_shipping_address_spec.rb
+++ spec/models/buy_shipping_address_spec.rb
@@ -22,6 +22,43 @@ RSpec.describe BuyShippingAddress, type: :model do
         @buy_shipping_address.valid?
         expect(@buy_shipping_address.errors.full_messages).to include("Post code can't be blank")
       end
+      it 'prefecture_idが空だと保存できない' do
+        @buy_shipping_address.prefecture_id = ''
+        @buy_shipping_address.valid?
+        expect(@buy_shipping_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'municipalityが空だと保存できない' do
+        @buy_shipping_address.municipality = ''
+        @buy_shipping_address.valid?
+        expect(@buy_shipping_address.errors.full_messages).to include("Municipality can't be blank")
+      end
+      it 'addressが空だと保存できない' do
+        @buy_shipping_address.address = ''
+        @buy_shipping_address.valid?
+        expect(@buy_shipping_address.errors.full_messages).to include("Address can't be blank")
+      end
+      it 'tel_numberが空だと保存できない' do
+        @buy_shipping_address.tel_number = ''
+        @buy_shipping_address.valid?
+        expect(@buy_shipping_address.errors.full_messages).to include("Tel number can't be blank")
+      end
+      it 'user_idが空だと保存できない' do
+        @buy_shipping_address.user_id = ''
+        @buy_shipping_address.valid?
+        expect(@buy_shipping_address.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_idが空だと保存できない' do
+        @buy_shipping_address.item_id = ''
+        @buy_shipping_address.valid?
+        expect(@buy_shipping_address.errors.full_messages).to include("Item can't be blank")
+      end
+      it 'buy_idが存在すると保存できない' do
+        @buy_shipping_address.item_id = '1'
+        @buy_shipping_address.valid?
+    
+        expect(@buy_shipping_address.errors.full_messages).to include("")
+      end
+
     end
   end
 end
