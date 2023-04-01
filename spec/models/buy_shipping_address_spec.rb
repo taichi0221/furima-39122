f25a832811a11d0717cc03bbf14bb2c715d152b9
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-04-01 19:03:14 +0900

    post_code空×

--- spec/models/buy_shipping_address_spec.rb
+++ spec/models/buy_shipping_address_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe BuyShippingAddress, type: :model do
       it 'post_code, prefecture_id, municipality, address, building, tel_numberが存在し,buy_idが空ならば保存できる' do
         expect(@buy_shipping_address).to be_valid
       end
+      it 'buildingは空でも保存できる' do
+        @buy_shipping_address.building = ''
+        expect(@buy_shipping_address).to be_valid
+      end
+    end
+
+    context '商品が購入できない場合' do
+      it 'post_codeが空だと保存できない' do
+        @buy_shipping_address.post_code = ''
+        @buy_shipping_address.valid?
+        expect(@buy_shipping_address.errors.full_messages).to include("Post code can't be blank")
+      end
     end
   end
 end
