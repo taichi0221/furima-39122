585b122a540b9e746cc7fc2bf504d77571ed8aa8
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-04-01 16:32:51 +0900

    createアクション、ストパラ

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -4,4 +4,20 @@ class BuysController < ApplicationController
     @item = Item.find(params[:item_id])
     @buy_shipping_address = BuyShippingAddress.new
   end
+
+  def create
+    @buy_shipping_address = BuyShippingAddress.new(buy_params)
+    if @buy_shipping_address.valid?
+      @buy_shipping_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def buy_params
+    params.require(:buy_shipping_address).permit(:post_code, :prefecture_id, :municipality, :address, :building, :tel_number)merge(item_id: item_id, user_id: current_user.id)
+
 end
