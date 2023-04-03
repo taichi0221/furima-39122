3f8acbca25571391ed060392118cb05350e211ff
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-04-03 11:16:44 +0900

    重複したエラーメッセージ解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,8 +14,7 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true,     numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_day_id, presence: true,   numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_charge_id, presence: true,numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true,             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  validates :price,                             numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, presence: { message: "can't be blank" }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is not a valid price" }
   validates :name, presence: true 
   validates :explanation , presence: true
   validates :image, presence: true
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -30,17 +30,17 @@ RSpec.describe Item, type: :model do
       it 'priceが半角数値以外では保存できない' do
         @item.price = '２０００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include("Price is not a valid price")
       end
       it 'priceが300未満では保存できない' do
         @item.price = '299'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include("Price is not a valid price")
       end
       it 'priceが10000000以上では保存できない' do
         @item.price = '10000000'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include("Price is not a valid price")
       end
       it 'category_idが1(表記：---)では保存できない' do
         @item.category_id = '1'
